a98781045bc9332a1c5adcaa0366bb9a4c0cbad8
Dizey5K <fedorovo727@gmail.com>
2025-02-06 12:44:58 +0300

    fixed navbar margin

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,29 @@
 // import ContainerGallery from './pages/HomeComponents/ContainerGallery'
 // import ContainerSale from './pages/HomeComponents/ContainerSale'
 // import { ContainerSecond } from './pages/HomeComponents/ContainerSecond'
-import AboutUs from './pages/AboutUs'
-import './App.scss'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
-import { NotFound } from './pages/NotFound'
+import AboutUs from "./pages/AboutUs";
+import "./scss/App.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home/>,
+    path: "/",
+    element: <Home />,
   },
   {
-    path: '/about',
-    element: <AboutUs/>,
+    path: "/about",
+    element: <AboutUs />,
   },
   {
-    path: '*',
-    element: <NotFound/>,
-  }
-])
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
 import ContainerContact from "./HomeComponents/ContainerContact";
-import { ContainerFirst } from "./HomeComponents/ContainerFirst";
 
 const AboutUs: React.FC = () => {
-    return (
-        <body>
-            <Navbar pageIndex={2}/> 
-            <ContainerFirst />
-            <ContainerContact />
-        </body>
-    )
-}
+  return (
+    <body>
+      <Navbar pageIndex={2} />
+      <ContainerContact />
+    </body>
+  );
+};
 
 export default AboutUs;
